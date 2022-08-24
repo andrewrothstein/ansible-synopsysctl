8d58ff39095f4aaeb66c8b860da788e0dd56926b
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-08-24 00:37:19 -0400

    synopsysctl:3.0.1

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -24,7 +24,7 @@ dl_ver() {
     local lchecksums=$DIR/synopsysctl-${ver}-checksums
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSlf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
@@ -35,4 +35,4 @@ dl_ver() {
     dl $ver windows amd64 zip
 }
 
-dl_ver ${1:-2.1.3}
+dl_ver ${1:-3.0.1}
